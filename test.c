0dbd32ad35fd4d3f3eaac71d9068a016a9888c35
Namrata R <37548876+Namratajjampur@users.noreply.github.com>
2020-02-12 16:34:34 +0530

    Checking removal of comments and spaces

---
+++ test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a =10;
+    /*jhi*/
+    //hello
+    while(a==10 || a<=3 && a!=5)
+    {
+        a++;
+        a+=3;
+        a= a/2;
+        //comm
+        /*hell
+        lslooe*/
+    }
+    return 0;
+    
+}
