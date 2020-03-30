4c3d9e4bdba02f1ed71462a966ad2b546f59ea2f
Diya1999 <40852957+Diya1999@users.noreply.github.com>
2020-03-30 22:03:12 +0530

    Create all_cases.c

---
+++ 30thMarchAST/all_cases.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+struct struct_name1 {
+	int member1;
+	float member2;
+}struct_id;
+
+int main()
+{
+{
+int a;
+int b;
+int c;
+}	
+while(a>0)
+{a++;}	
+switch(b)
+{
+case 1:b=b+1;
+default:b=b+9;
+}
+}
