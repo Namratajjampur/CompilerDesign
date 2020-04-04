314a352f15ce2c96321a4d0e4931945dfef3a7f4
Diya1999 <40852957+Diya1999@users.noreply.github.com>
2020-04-04 15:41:27 +0530

    Create all_casesast.c

---
+++ 4thApril20/all_casesast.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+struct struct_name1 {
+	int member1;
+	float member2;
+	char d;
+}h;
+struct struct_name2{
+	int member1;
+	float member2;
+	char d;
+}struct_id2,struct_id8;
+int d,f;
+int main()
+{
+int a;
+struct_id2.member2=a+b;
+struct struct_name1 hat;
+//int a,c=6;
+//int b[2];
+}
