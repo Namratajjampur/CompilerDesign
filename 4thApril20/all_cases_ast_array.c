e9d12aaad09d30252f7cfcde334ac17d011bb219
Namrata R <37548876+Namratajjampur@users.noreply.github.com>
2020-04-04 23:02:08 +0530

    Create all_cases_ast_array.c

---
+++ 4thApril20/all_cases_ast_array.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+struct name1{
+int a;//1
+int b[10][9][17];
+int c;
+}name[10][2],nameeer;
+
+struct nameaa{
+int a;//1
+int g;
+
+}namerr;
+int main()
+{
+/*int addd;
+ struct name1 name2,name3;
+ struct nameaa abc;
+	struct nameaa ccbd;
+	name2.a = 45;
+	abc.g=34;
+	name.c= name2.a-abc.g;
+	name.c++;
+	int abcdefg;
+*/
+
+int l;
+//name.a =6;
+int l[2][9];
+l[1][3]=4;
+}
