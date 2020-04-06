36984aa33af72b921078bcb91ba52ed86fa1c5a8
Namrata R <37548876+Namratajjampur@users.noreply.github.com>
2020-04-06 20:01:13 +0530

    all types of nested tried and tested

---
+++ 6thApril/nested_structures_cases.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+struct name{
+//int member1[10][20];
+//char kll[10][90];
+int member2[10][8],kll;
+//int o;
+}name1;
+
+struct name2{
+int member3;
+int jj;
+struct name name90;
+} name4;
+
+
+
+
+int main()
+{
+ //fgh =0;
+int a =2;
+struct abc
+{
+int gg; int hh;}kk;
+struct abc uu;
+struct name name3;
+int ggk[10];
+struct name2 hnamh;
+name4.name90.kll = 100;
+	//struct name app[10],mem;
+	//yyfg.member1=9;
+	//int a[2][3][4];
+	//name4.name3.member1=8;
+	//name.name90.member2 =44;
+//name1.o =a+1;
+//name1.member2 = name1.o+34;
+//name4.num.mem = name.name90.member2 +1;
+//lmno =9090;
+
+hnamh.name90.kll = name4.name90.kll+a;
+}
