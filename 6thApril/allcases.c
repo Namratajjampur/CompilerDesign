9ebb2af6278748de0a261ef555bf7e3e75b53d9a
Diya1999 <40852957+Diya1999@users.noreply.github.com>
2020-04-06 14:29:38 +0530

    s

---
+++ 6thApril/allcases.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{	
+	struct_id.mem=1+2;
+	/*switch(a)
+	{
+	case 4: a++;
+	break;
+	case 5: b--;
+		switch(b)
+		{case 4: a++;
+			break;
+		default:b--;
+		}
+	case 6: a--;
+	default: b++;}*/
+
+	switch(b)
+		{case 4: a++;
+			break;
+		default:b--;
+		}
+	switch(a)
+		{case 3: a++;
+		 case 4: a--;
+			/*while(a>0){a--;}
+			break;
+		default:b--;*/
+		}
+
+}
