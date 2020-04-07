a2510a7e92bdbbed10494727f5be972ac0404fca
Diya1999 <40852957+Diya1999@users.noreply.github.com>
2020-04-07 12:38:38 +0530

    Create allcases_ast.c

---
+++ FinalAll/allcases_ast.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+struct struct_name1 {
+	int member1[10];
+	float member2,b;
+	char d;
+}h;
+struct struct_name2{
+	int member1;
+	float member2;
+	char d;
+}struct_id2,struct_id8;
+int d,f;
+int main()
+{
+int a=1;
+struct_id2.member2=a+b;
+struct struct_name1 hat;
+/*switch(a)
+{
+case 1: a++;
+break;
+case 2:a--;
+default: a=a+1;
+}*/
+/*
+while(a>0)
+{
+	a--;
+}*/
+//int b[10];
+}
